572c20d371929c77925a8f7bd618ada56d1a9036
Ünver Can Ünlü <unvercanunlu@gmail.com>
2022-05-05 22:26:11 +0300

    cart app is added.

---
+++ cart/src/main/java/tr/unvercanunlu/ecommerce/cart/CartApp.java
@@ -0,0 +1,12 @@
+package tr.unvercanunlu.ecommerce.cart;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CartApp {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CartApp.class, args);
+	}
+}
---
+++ cart/src/test/java/tr/unvercanunlu/ecommerce/cart/CartAppTest.java
@@ -0,0 +1,12 @@
+package tr.unvercanunlu.ecommerce.cart;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CartAppTest {
+
+	@Test
+	void contextLoads() {
+	}
+}
