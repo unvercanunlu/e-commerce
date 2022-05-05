d93ddf9ade8d48bd28e826abc14b7c01bf548bce
Ünver Can Ünlü <unvercanunlu@gmail.com>
2022-05-05 22:27:17 +0300

    ui app is added.

---
+++ ui/src/main/java/tr/unvercanunlu/ecommerce/ui/UiApp.java
@@ -0,0 +1,12 @@
+package tr.unvercanunlu.ecommerce.ui;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UiApp {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UiApp.class, args);
+	}
+}
---
+++ ui/src/test/java/tr/unvercanunlu/ecommerce/ui/UiAppTest.java
@@ -0,0 +1,12 @@
+package tr.unvercanunlu.ecommerce.ui;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UiAppTest {
+
+	@Test
+	void contextLoads() {
+	}
+}
